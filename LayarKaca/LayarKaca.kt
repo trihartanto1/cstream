af7d1aac64088114f5e93765e585188049359f8e
trihartanto1 <80317647+trihartanto1@users.noreply.github.com>
2025-06-01 12:33:47 +0700

    Update LayarKaca.kt

--- LayarKaca/LayarKaca.kt
+++ LayarKaca/LayarKaca.kt
@@ -1,10 +1,12 @@
 package com.trihartanto1
 
-import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.TvType
 
-class IndoXXI : MainAPI() {
+class LayarKaca : MainAPI() {
     override var mainUrl = "https://lk21.film/"
     override var name = "LayarKaca"
-    override val supportedTypes = setOf(TvType.Movie)
+    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
+
+    // Tambahkan logika scrapper sesuai kebutuhan di sini
 }
