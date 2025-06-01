313659107f6bba4f8167215d667c76074b2acf95
trihartanto1 <80317647+trihartanto1@users.noreply.github.com>
2025-06-01 12:33:05 +0700

    Update build.gradle.kts

--- LayarKaca/build.gradle.kts
+++ LayarKaca/build.gradle.kts
@@ -1,27 +1,9 @@
-// use an integer for version numbers
-version = 20
-
+plugins {
+    id("cloudstream-plugin")
+}
 
 cloudstream {
     language.set("id")
     description.set("Plugin LayarKaca")
     authors.set(listOf("trihartanto1"))
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-     * */
-    status = 1 // will be 3 if unspecified
-    tvTypes = listOf(
-        "AsianDrama",
-        "TvSeries",
-        "Movie",
-    )
-
-
-    iconUrl = "https://www.google.com/s2/favicons?domain=amp.lk21official.mom&sz=%size%"
-
 }
